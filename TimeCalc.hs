667825262594ff4c79f644156ec2a0eb12cc3a2b
Chris Eidhof <ce@tupil.com>
2011-05-02 17:25:05 +0200

    First revision

---
+++ TimeCalc.hs
@@ -0,0 +1,60 @@
+module TimeCalc where
+
+import Text.ParserCombinators.UU
+import Text.ParserCombinators.UU.Utils
+import Text.ParserCombinators.UU.BasicInstances hiding (Parser, input)
+import System.Console.Haskeline
+import System.Environment (getArgs)
+
+type Parser a = P (Str Char String LineColPos) a
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if null args then interactive 
+               else putStrLn $ run timeExpr (unwords args)
+
+interactive :: IO ()
+interactive = runInputT defaultSettings loop
+  where 
+   loop :: InputT IO ()
+   loop = do minput <- getInputLine "> "
+             case minput of
+               Nothing -> return ()
+               Just "quit" -> return ()
+               Just input -> do outputStrLn $ run timeExpr input
+                                loop
+
+
+run :: Show t =>  Parser t -> String -> String
+run p inp = do  let (a, errors) =  parse ( (,) <$> p <*> pEnd) (createStr (LineColPos 0 0 0) inp)
+                if null errors then  show a
+                               else  "Error in expression"
+
+timeExpr :: Parser String
+timeExpr = format <$> expr
+ where format :: Double -> String
+       format x = let minutes :: Integer
+                      seconds :: Integer
+                      (minutes,secondsMultiplier) = properFraction x
+                      seconds = round $ secondsMultiplier * 60
+                      showSeconds s | s < 10    = "0" ++ show s
+                                    | otherwise = show s
+                  in  show minutes ++ ":" ++ showSeconds seconds
+
+expr :: Parser Double
+expr = foldr pChainl ( pDouble <|> pTime <|>pParens expr) (map same_prio operators) 
+ where
+  operators       = [[('+', (+)), ('-', (-))],  [('*' , (*))], [('/' , (/))]]
+  same_prio  ops  = foldr (<|>) empty [ op <$ pSym c | (c, op) <- ops]
+
+pTime :: Parser Double
+pTime = pMinuteTime <|> pHourTime
+
+pMinuteTime :: Parser Double
+pMinuteTime = makeTime <$> pIntegerRaw <* pSym ':' <*> pIntegerRaw <?> "min:sec"
+ where makeTime x y = x + (y / 60.0)
+
+pHourTime :: Parser Double
+pHourTime = makeTime <$> pIntegerRaw <* pSym ':' <*> pIntegerRaw <* pSym ':' <*> pIntegerRaw <?> "min:sec"
+ where makeTime x y z = x * 60 + y + z / 60.0
